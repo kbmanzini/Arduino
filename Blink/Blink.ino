4a8d9d8da6d89c6274f929f108b0674c09183a29
Oliver Gibbs <02oliverg.64@gmail.com>
2025-04-22 13:26:36 -0400

    Red LED Code

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -20,18 +20,27 @@
   This example code is in the public domain.
 
   https://docs.arduino.cc/built-in-examples/basics/Blink/
+
 */
+
+int blueLED = 12;
+int redLED = 11;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(12, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(blueLED, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(3000);                      // wait for a second
+  digitalWrite(blueLED, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
-  digitalWrite(12, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);                      // wait for a second
+  digitalWrite(redLED, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(3000);                      // wait for a second
+  digitalWrite(redLED, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
 }
